afa2f5bef08c2b8dd414ede9b9bd8bf7001bb168
donna <donnacamos88@gmail.com>
2019-10-22 12:13:50 -0400

    Done.

--- index.js
+++ index.js
@@ -3,57 +3,55 @@
 function mapToNegativize(src) {
     let r = []
     for (let i = 0; i < src.length; i++ ) {
-      r.push(-1 * src[i]) // Unique work
+      r.push(-1 * src[i])
     }
     return r
   }
-   
+  
   function mapToNoChange(src) {
     let r = []
     for (let i = 0; i < src.length; i++ ) {
-      r.push(src[i]) // Unique work
+      r.push(src[i])
     }
     return r
   }
-   
+  
   function mapToDouble(src) {
     let r = []
     for (let i = 0; i < src.length; i++ ) {
-      r.push(2 * src[i]) // Unique work
+      r.push(2 * src[i])
     }
     return r
   }
-   
+  
   function mapToSquare(src) {
     let r = []
     for (let i = 0; i < src.length; i++ ) {
-      r.push(src[i] * src[i]) // Unique work
+      r.push(src[i] * src[i])
     }
     return r
   }
-
-  function reduceToTotal(src) {
-      let r = [] 
-      for (let i = 0; i < src.length; i++) {
-          r.push(src[i] < 6 ) 
-      }
-      return r 
+  
+  function reduceToTotal(src, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i < src.length; i++ ) {
+      total = total + src[i]
+    }
+    return total
   }
-
+  
   function reduceToAllTrue(src) {
-    let r = []
-    for (let i = 0; i < src.length; i++) {
-        r.push(src[i]) 
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
     }
-    return r
+    return true
   }
-
+  
   function reduceToAnyTrue(src) {
-      let r = [] 
-      for (let i = 0; i < src.length; i++) {
-          r.push(src[i])
-      }
-      return r 
+    for (let i = 0; i < src.length; i++ ) {
+      if (src[i]) return true
+    }
+    return false
   }
    
  
