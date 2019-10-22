01cccb87dcf1020d97fcf98d558a974595a9ba60
donna <donnacamos88@gmail.com>
2019-10-22 12:12:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Your code here
+
+function mapToNegativize(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(-1 * src[i]) // Unique work
+    }
+    return r
+  }
+   
+  function mapToNoChange(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(src[i]) // Unique work
+    }
+    return r
+  }
+   
+  function mapToDouble(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(2 * src[i]) // Unique work
+    }
+    return r
+  }
+   
+  function mapToSquare(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++ ) {
+      r.push(src[i] * src[i]) // Unique work
+    }
+    return r
+  }
+
+  function reduceToTotal(src) {
+      let r = [] 
+      for (let i = 0; i < src.length; i++) {
+          r.push(src[i] < 6 ) 
+      }
+      return r 
+  }
+
+  function reduceToAllTrue(src) {
+    let r = []
+    for (let i = 0; i < src.length; i++) {
+        r.push(src[i]) 
+    }
+    return r
+  }
+
+  function reduceToAnyTrue(src) {
+      let r = [] 
+      for (let i = 0; i < src.length; i++) {
+          r.push(src[i])
+      }
+      return r 
+  }
+   
+ 
+
