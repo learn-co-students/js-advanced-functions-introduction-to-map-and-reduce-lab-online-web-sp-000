f8e7746f034e42a3b6093bc602653c1543fcbd1e
Bry Cox <carl.cc@gmail.com>
2019-11-22 11:27:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+function mapToNegativize(arr) {
+  for(const e in arr) {
+    arr[e]*= -1;
+  }
+  return arr
+}
+
+function mapToDouble(arr) {
+  for(const e in arr) {
+    arr[e]*= 2;
+  }
+  return arr
+}
+
+function mapToNoChange(arr) {
+  return arr
+}
+
+function mapToSquare(arr) {
+  for(const e in arr) {
+    arr[e]**= 2;
+  }
+  return arr
+}
+
+function mapToNegativize(arr) {
+  for(const e in arr) {
+    arr[e]*= -1;
+  }
+  return arr
+}
+
+function reduceToTotal(arr, start = 0) {
+  for(const e of arr) {
+    start += e;
+  }
+  return start;
+}
+
+function reduceToAllTrue(arr) {
+  for(const e of arr) {
+    if(!e) { return false; }
+  }
+  return true;
+}
+
+function reduceToAnyTrue(arr) {
+  for(const e of arr) {
+    if(e) { return true; }
+  }
+  return false;
+}
