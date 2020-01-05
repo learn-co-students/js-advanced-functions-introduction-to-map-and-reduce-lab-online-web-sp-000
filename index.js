69b01b0aaa6509953fb0da501f75eae1cd73d96e
stabassum <sumu1993@gmail.com>
2020-01-04 23:57:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+
+function  mapToNegativize(sourceArray){
+
+}
+
+function mapToNoChange(sourceArray){
+
+}
+
+function mapToDouble(sourceArray){
+
+}
+
+function mapToSquare(sourceArray){
+
+}
+
+function reduceToTotal(sourceArray, startingPoint){
+
+}
+
+function reduceToAllTrue(sourceArray){
+
+}
+
+function reduceToAnyTrue(sourceArray){
+
+}
