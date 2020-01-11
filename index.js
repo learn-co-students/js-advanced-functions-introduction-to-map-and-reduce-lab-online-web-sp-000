2174423cd803bdba950667bd414af09ce22d34e3
morganpstanley <morganpstanley@gmail.com>
2020-01-11 02:12:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+function mapToNegativize(array) {
+    let returnArray = [];
+    array.forEach(element => {
+        returnArray.push(element * -1)
+    });
+    return returnArray
+}
+
+function mapToNoChange(array) {
+    return array
+}
+
+function mapToDouble(array) {
+    let returnArray = [];
+    array.forEach(element => {
+        returnArray.push(element * 2)
+    });
+    return returnArray
+}
+
+function mapToSquare(array) {
+    let returnArray = [];
+    array.forEach(element => {
+        returnArray.push(element * element)
+    });
+    return returnArray
+}
+
+function reduceToTotal(array, num = 0) {
+    array.forEach(element => {
+        num += element
+    })
+    return num;
+}
+
+function reduceToAllTrue(array) {
+    let results;
+    array.forEach(element => {
+        if (!element === true) {
+            results = false
+        }
+    })
+    return results === false ? false : true
+}
+
+function reduceToAnyTrue(array) {
+    let results;
+    array.forEach(element => {
+        if (!!element === true) {
+            results = true;
+        }
+    })
+    return results === true ? true : false
+}
