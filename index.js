173aac0ce3ff5e65338ca04a231d924e6e3d9c5d
BlythePollard <github email address>
2020-03-23 20:53:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+function mapToNegativize(array) {
+    const newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i] * -1)
+    }
+    return newArray
+}
+
+function mapToNoChange(array) {
+    const newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i])
+    }
+    return newArray
+}
+
+function mapToDouble(array) {
+    const newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i] * 2)
+    }
+    return newArray
+}
+
+function mapToSquare(array) {
+    const newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(array[i] * array[i])
+    }
+    return newArray
+}
+
+function reduceToTotal(array, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i < array.length; i++) {
+        total = total + array[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(array) {
+    for (let i = 0; i < array.length; i++) {
+       if(!array[i]) {
+           return false
+       }
+    }
+    return true 
+}
+
+function reduceToAnyTrue(array) {
+    for (let i = 0; i < array.length; i++) { 
+        if(!!array[i]) {
+            return true
+        }
+    }
+    return false
+}
