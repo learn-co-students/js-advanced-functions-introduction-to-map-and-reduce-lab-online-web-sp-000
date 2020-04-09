591894dd2502f8ce0959888fa5c2a2a0bb8c5aa6
beckyphan <github email address>
2020-04-09 14:18:51 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Your code here
+// mapping a value in the domain to a value in the range
+function mapToNegativize(sourceArray) {
+  let array = []
+  for (const element of sourceArray) {
+    array.push(element * -1)
+  }
+  return array
+}
+
+function mapToNoChange(sourceArray) {
+  let array = sourceArray
+  return array
+}
+
+function mapToDouble(sourceArray) {
+  let array = []
+  for (const element of sourceArray) {
+    array.push(element * 2)
+  }
+  return array
+}
+
+function mapToSquare(sourceArray) {
+  let array = []
+  for (const element of sourceArray) {
+    array.push(element * element)
+  }
+  return array
+}
+
+// does work to a memo or aggregator
+function reduceToTotal(sourceArray, startingPoint) {
+  let total = startingPoint ? startingPoint : 0
+  for (const element of sourceArray) {
+    total += element
+  }
+  return total
+}
+function reduceToAllTrue(sourceArray) {
+  let status = true
+  for (const element of sourceArray) {
+    if (element == false) {
+      status = false
+    }
+  }
+  return status
+}
+
+function reduceToAnyTrue(sourceArray) {
+  let status = false
+  for (const element of sourceArray) {
+    if (element == true) {
+      status = true
+    }
+  }
+  return status
+}
