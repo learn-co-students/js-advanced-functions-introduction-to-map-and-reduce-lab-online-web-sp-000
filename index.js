7b93522eaa38d63cc1afd8bbc8e6ea91b0a06f5b
github username <github email address>
2020-06-19 19:27:20 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+const mapToNegativize = myArray => {
+    return myArray.map(newArray => newArray * -1)
+}
+
+const mapToNoChange = myArray => {
+    return myArray.map(newArray => newArray)
+}
+
+const mapToDouble = myArray => {
+    return myArray.map(newArray => newArray * 2)
+}
+
+const mapToSquare = myArray => {
+    return myArray.map(newArray => newArray ** 2)
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    return sourceArray.reduce((a,c) => {
+        a = a + c
+        return a
+    }, startingPoint
+    )
+}
+
+function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++ ){
+        if (!sourceArray[i]) return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < src.length; i++) {
+        if (src[i]) return true
+    }
+    return false
+}
