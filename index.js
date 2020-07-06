df56090029e249388fd7a057f6f7d0e57fdad276
Dan-zai <danielcowpertwait@gmail.com>
2020-07-06 11:49:44 +1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function mapToNegativize(array){
+    return array.map(x => x * -1);
+}
+
+function mapToNoChange(array){
+    return array.map(x => x);
+}
+
+function mapToDouble(array){
+    return array.map(x => x*2);
+}
+
+function mapToSquare(array){
+    return array.map(x => x*x);
+}
+
+function reduceToTotal(numbers, startingPoint = 0) {
+    return numbers.reduce(
+      function(total, currentValue) {
+        return total + currentValue}, startingPoint)
+  }
+  
+  function reduceToAllTrue(numbers) {
+      return numbers.reduce(function(total, currentValue) {
+          if (currentValue) {
+              return true
+          } else {
+              return false
+          }
+      })
+  }
+  
+  function reduceToAnyTrue(numbers) {
+      return numbers.reduce(function(total, currentValue) {
+          if (currentValue) {
+              return true
+          } else {
+              return false
+          }
+      })
+  }
