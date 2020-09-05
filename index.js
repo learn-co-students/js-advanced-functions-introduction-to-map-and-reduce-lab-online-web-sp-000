ac8640b0376d9fd082307a02877f6e9d10df3e5c
connorworthen <connorw220@protonmail.com>
2020-09-05 14:20:19 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+function mapToNegativize(array) {
+  let a = [];
+  for (let i = 0; i < array.length; i++) {
+    a.push(-1 * array[i]);
+  }
+  return a;
+}
+
+function mapToNoChange(array) {
+  let a = [];
+  for (let i = 0; i < array.length; i++) {
+    a.push(array[i]);
+  }
+  return a;
+}
+
+function mapToDouble(array) {
+  let a = [];
+  for (let i = 0; i < array.length; i++) {
+    a.push(2 * array[i]);
+  }
+  return a;
+}
+
+function mapToSquare(array) {
+  let a = [];
+  for (let i = 0; i < array.length; i++) {
+    a.push(array[i] * array[i]);
+  }
+  return a;
+}
