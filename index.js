fa7d05b0d6f311bcd5fc84d02973cd46acf4123d
Elliot Blanchard <git@elliotblanchard.com>
2020-09-09 20:51:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
+const { should } = require("chai");
+
 // Your code here
+function mapToNegativize(sourceArray) {
+    let returnArray = [];
+
+    for (var i = 0; i < sourceArray.length; i++) {
+        returnArray.push(sourceArray[i] * -1); 
+    }
+
+    return returnArray;
+}
+
+function mapToNoChange(sourceArray) {
+    let returnArray = [];
+
+    for (var i = 0; i < sourceArray.length; i++) {
+        returnArray.push(sourceArray[i]); 
+    }
+
+    return returnArray;
+}
+
+function mapToDouble(sourceArray) {
+    let returnArray = [];
+
+    for (var i = 0; i < sourceArray.length; i++) {
+        returnArray.push(sourceArray[i]*2); 
+    }
+
+    return returnArray;
+}
+
+function mapToSquare(sourceArray) {
+    let returnArray = [];
+
+    for (var i = 0; i < sourceArray.length; i++) {
+        returnArray.push(sourceArray[i]**2); 
+    }
+
+    return returnArray;
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    let returnValue = startingPoint;
+
+    for (var i = 0; i < sourceArray.length; i++) {
+        returnValue += sourceArray[i]; 
+    }
+
+    return returnValue;
+}
+
+function reduceToAllTrue(sourceArray) {
+    let returnValue = false;
+
+    for (var i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i]) {
+            returnValue = true;
+        } 
+        else {
+            returnValue = false;
+            break;
+        }
+    }
+    
+    return returnValue;
+}
+
+function reduceToAnyTrue(sourceArray) {
+    let returnValue = false;
+
+    for (var i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i]) {
+            returnValue = true;
+            break;
+        } 
+        else {
+            returnValue = false;
+        }
+    }
+    
+    return returnValue;
+}
