b24d3dc418206d4ae4ee0ba70734a4c24ce37ab1
Indre Bakeer <indre.bakeer@gmail.com>
2020-10-14 12:04:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  let array = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    array.push(-1 * sourceArray[i])
+  }
+  return array;
+}
+
+function mapToNoChange(sourceArray) {
+  let array = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    array.push(sourceArray[i])
+  }
+  return array;
+}
+
+function mapToDouble(sourceArray) {
+  let array = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    array.push(2 * sourceArray[i])
+  }
+  return array;
+}
+
+function mapToSquare(sourceArray) {
+  let array = []
+  for (let i = 0; i < sourceArray.length; i++) {
+    array.push(sourceArray[i] * sourceArray[i])
+  }
+  return array;
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    startingPoint = startingPoint + sourceArray[i]
+  }
+  return startingPoint;
+}
+
+function reduceToAllTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (!sourceArray[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+  for (let i = 0; i < sourceArray.length; i++) {
+    if (sourceArray[i]) return true
+  }
+  return false
+}
