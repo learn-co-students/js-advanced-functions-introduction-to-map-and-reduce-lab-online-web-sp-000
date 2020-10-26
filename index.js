758bfe1fabb3d2f35cd1a26b8a6e8d85b4719e51
rohanphillips <rohan@solutions-insurance.com>
2020-10-26 13:38:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+
+function mapToNegativize(sourceArray){
+  return sourceArray.map(function(num){
+    return num * -1;
+  })
+}
+
+function mapToNoChange(sourceArray){
+  return sourceArray.map(function(item){
+    return item;
+  });
+}
+
+function mapToDouble(sourceArray){
+  return sourceArray.map(function(num){
+    return num * 2;
+  })
+}
+
+function mapToSquare(sourceArray){
+  return sourceArray.map(function(num){
+    return num ** 2;
+  })
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+  return startingPoint +++ sourceArray.reduce(function(total, num){
+    return total + num;
+  })
+}
+
+function reduceToAllTrue(sourceArray){
+  let isTrue = true;
+  for (let i = 0; i < sourceArray.length; i++){
+    console.log()
+    if (sourceArray[i] == false){
+      isTrue = false;
+    }
+  }
+  return isTrue;
+}
+
+function reduceToAnyTrue(sourceArray){
+  let isTrue = false;
+  for (let i = 0; i < sourceArray.length; i++){
+    console.log()
+    if (sourceArray[i] == true){
+      isTrue = true;
+      break;
+    }
+  }
+  return isTrue;
+}
