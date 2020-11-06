008eb40d5ea60c7a6c97b998b61ea4a2c6d5eb00
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-06 07:51:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function mapToNegativize(array) {
+  return array.map(x => x*-1)
+};
+
+function mapToNoChange(array) {
+  return array;
+}
+
+function mapToDouble(array) {
+  return array.map(x => x * 2);
+};
+
+function mapToSquare(array) {
+  return array.map(x => x * x)
+}
+
+function reduceToTotal(array) {
+  const answer = array.reduce((acc, val) => acc + val);
+  return answer;
+};
+
+function reduceToAllTrue(array) {
+  const answer = array.reduce((val) => Boolean(val));
+  return answer;
+};
