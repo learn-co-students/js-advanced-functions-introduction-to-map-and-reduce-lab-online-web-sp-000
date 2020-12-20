9006b4e30cd1b1a6482dbdd8b92cf213c3cdb893
CoderJay06 <jaycru06@yahoo.com>
2020-12-20 02:06:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+/*
+map-like
+mapToNegativize(sourceArray)
+mapToNoChange(sourceArray)
+mapToDouble(sourceArray)
+mapToSquare(sourceArray)
+Remember, all map methods return a new Array.
+
+reduce-like
+reduceToTotal(sourceArray, startingPoint)
+reduceToAllTrue(sourceArray)
+reduceToAnyTrue(sourceArray)
+ */
+
+const mapToNegativize = (sourceArray) => {
+    // make all values in sourceArray negative
+    const negativesArray = []
+    sourceArray.forEach(value => {
+        if (value > 0) {
+            // convert postives to negative
+            negativesArray.push(value - (value * 2))
+        } else {
+            // convert negatives to positives
+            negativesArray.push(value * -1)
+        }
+    })
+    return negativesArray
+}
