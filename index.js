4e3cfe25eb493a4e42aa7c901494f778f938ec9d
NicholasR77 <nl7robbins@icloud.com>
2021-01-01 23:07:59 -0700

    Done.

--- index.js
+++ index.js
@@ -16,26 +16,3 @@ function mapToDouble(sourceArray) {
 function mapToSquare(sourceArray) {
     return sourceArray.map(arr => arr * arr);
 }
-
-
-function reduceToTotal(src, startingPoint=0) {
-  let total = startingPoint
-  for (let i = 0; i < src.length; i++ ) {
-    total = total + src[i]
-  }
-  return total
-}
-
-function reduceToAllTrue(src) {
-  for (let i = 0; i < src.length; i++ ) {
-    if (!src[i]) return false
-  }
-  return true
-}
-
-function reduceToAnyTrue(src) {
-  for (let i = 0; i < src.length; i++ ) {
-    if (src[i]) return true
-  }
-  return false
-}
