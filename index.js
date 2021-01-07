308480aa8426f0cb1037b29671cb8aa502c3fdec
Gearz <shanegeary@yahoo.com>
2021-01-07 15:07:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(arr => arr * -1);
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map(arr => arr);
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(arr => arr * 2);
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map(arr => arr * arr);
+}
+
+function reduceToTotal(sourceArray, startingPoint = null) {
+    const reducer = (accumulator, value) => accumulator + value;
+        return sourceArray.reduce(reducer, startingPoint);
+}
+
+function reduceToAllTrue(sourceArray) {
+    const truthyValue = (currentValue) => currentValue ? true : false;
+    return sourceArray.every(truthyValue);
+}
+
+function reduceToAnyTrue(sourceArray) {
+    const someTrue = (currentValue) => currentValue ? true : false;
+    return sourceArray.some(someTrue);
+}
