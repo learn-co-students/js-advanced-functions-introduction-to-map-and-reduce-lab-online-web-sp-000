3b24e86099ad81aa4120c6e63faaad45b9f429c5
anpham148 <anpham148@gmail.com>
2021-02-12 11:25:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,47 +1,76 @@
 // Your code here
 function mapToNegativize(sourceArray) {
-    return sourceArray.map(x => x*(-1))
+    let r = [];
+    for (let i = 0; i < sourceArray.length; i++) {
+        r.push(sourceArray[i]*(-1))
+    };
+    return r
 }
 
 function mapToNoChange(sourceArray) {
-    return sourceArray.map(x => x)
+    let r = [];
+    for (let i = 0; i < sourceArray.length; i++) {
+        r.push(sourceArray[i])
+    };
+    return r
 }
 
 function mapToDouble(sourceArray) {
-    return sourceArray.map(x => x*2)
+    let r = [];
+    for (let i = 0; i < sourceArray.length; i++) {
+        r.push(sourceArray[i] * 2)
+    };
+    return r
 }
 
 function mapToSquare(sourceArray) {
-    return sourceArray.map(x => x**2)
+    let r = [];
+    for (let i = 0; i < sourceArray.length; i++) {
+        r.push(sourceArray[i]**2)
+    };
+    return r
 }
 
 const reducer = (accumulator, currentVal) => accumulator + currentVal;
 
-function reduceToTotal(sourceArray, startingPoint) {
-    if (startingPoint) {
-        return sourceArray.reduce(reducer, startingPoint);
-    } else {
-        return sourceArray.reduce(reducer);
-    }
+function reduceToTotal(sourceArray, startingPoint=0) {
+    let total = startingPoint;
+    for (let i = 0; i < sourceArray.length; i++) {
+      total = total + sourceArray[i];
+    };
+    return total;
 }
 
 function reduceToAllTrue(sourceArray) {
-   return sourceArray.reduce((accumulator, currentValue) => {
-       if (accumulator) {
-        return accumulator = !!currentValue
-       }
-       return accumulator
-   })
+
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!sourceArray[i]) {
+            return false
+        }
+    }
+    return true;
+//    return sourceArray.reduce((accumulator, currentValue) => {
+//        if (accumulator) {
+//         return accumulator = !!currentValue
+//        }
+//        return accumulator
+//    })
 }
 
 function reduceToAnyTrue(sourceArray) {
-    return sourceArray.reduce((accumulator, currentValue) => {
-     console.log("acc", accumulator)
-     console.log(currentValue)
-     console.log(!!currentValue)
-        if (!accumulator) {
-         return accumulator = !!currentValue
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i]) {
+            return true;
         }
-        return accumulator
-    })
+    }
+    return false;
+    // return sourceArray.reduce((accumulator, currentValue) => {
+    //  console.log("acc", accumulator)
+    //  console.log(currentValue)
+    //  console.log(!!currentValue)
+    //     if (!accumulator) {
+    //      return accumulator = !!currentValue
+    //     }
+    //     return accumulator
+    // })
  }
