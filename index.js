80239241a4717ff9b0ae28dfee7fc3cdb8687062
john million <jpmillion@comcast.net>
2021-02-18 15:42:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+function mapToNegativize(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] *= -1;
+    }
+    return array;
+}
+
+function mapToNoChange(array) {
+    return array;
+}
+
+function mapToDouble(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] *= 2;
+    }
+    return array;
+}
+
+function mapToSquare(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = array[i]**2;
+    }
+    return array;
+}
+
+function reduceToTotal(array, total=0) {
+    for (const el of array) {
+        total += el;
+    }
+    return total;
+}
+
+function reduceToAllTrue(array) {
+    for (const el of array) {
+        if (!el) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function reduceToAnyTrue(array) {
+    for (const el of array) {
+        if (el) {
+            return true;
+        }
+    }
+    return false;
+}
