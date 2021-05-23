a0499094a62c9d751505e206ec860d36183d134a
bongofury <jtroutmanman@gmail.com>
2021-05-23 17:39:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let result = [];
+    sourceArray.forEach(element => {
+        result.push(element * -1);
+    })
+    return result;
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray;
+}
+
+function mapToDouble(sourceArray) {
+    let result = [];
+    sourceArray.forEach(element => {
+        result.push(element * 2);
+    });
+    return result;
+}
+
+function mapToSquare(sourceArray) {
+    let result = [];
+    sourceArray.forEach(element => {
+        result.push(element ** 2);
+    });
+    return result;
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    sourceArray.forEach(element => {
+        startingPoint += element;
+    });
+    return startingPoint;
+}
+
+function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!!sourceArray[i] === false) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!!sourceArray[i] === true) {
+            return true;
+        }
+    }
+    return false;
+}
